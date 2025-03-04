a2703f471f52ed567271947947036ae1abd242e2
Yimeng Zeng <yimengz@seas.upenn.edu>
2025-03-04 01:31:49 -0500

    fix submission script

--- optimization/constrained_bo_scripts/optimize_gramnegative_only.sh
+++ optimization/constrained_bo_scripts/optimize_gramnegative_only.sh
@@ -4,14 +4,14 @@ python apex_oracle_constrained_diverse_optimization.py \
     --max_n_oracle_calls 1000000 \
     --bsz 50 \
     --track_with_wandb True \
-    --constraint_function_ids [similarity] \
-    --constraint_thresholds [0.75] \
-    --constraint_types [GHLLIHLIGKATLAL] \
+    --constraint_function_ids "[similarity]" \
+    --constraint_thresholds "[0.75]" \
+    --constraint_types "[GHLLIHLIGKATLAL]" \
     --wandb_entity YOUR_WANDB_ENTITY \
     --num_initialization_points 10000 \
     --dim 256 \
     --max_string_length 30 \
     --M 20 \
     --tau 1 \
-    --task_specific_args [gramnegonly_mean] \
+    --task_specific_args "[gramnegonly_mean]" \
     - run_robot - done
